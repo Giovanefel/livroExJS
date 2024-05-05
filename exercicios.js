603030f0e47f89cc7baabf39f1a816873358e685
Giovane <giovanefe17@gmail.com>
2024-05-05 12:11:43 -0300

    exercicio
    add js

---
+++ exercicios.js
@@ -0,0 +1,7 @@
+/*function mostrarOla() {
+    var nome = document.getElementById("nome").value
+    document.getElementById("resposta").innerHTML = "Ola"+ nome;
+
+}
+var mostrar = document.getElementById("mostrar");
+mostrar.onclick=mostrarOla;*/
