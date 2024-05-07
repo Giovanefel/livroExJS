d25788ca83f7d8d1ecf062237a7257d8dfb4d2e4
Giovane <giovanefe17@gmail.com>
2024-05-06 22:25:36 -0300

    alteraçoes js
    add mais no js

--- exercicios.js
+++ exercicios.js
@@ -5,3 +5,16 @@
 }
 var mostrar = document.getElementById("mostrar");
 mostrar.onclick=mostrarOla;*/
+
+// declara a funçao mostrarOla
+function mostrarOla(){
+    //obtem o conteudo do campo(com id=)nome
+    var nome = document.getElementById("nome").value;
+    //exibe o paragrafo (resposta):"ola" e o nome informado
+    document.getElementById("resposta"). textContent = "Olá " + nome;
+}
+//cria uma referencia so botao (com id=)mostrar
+var mostrar = document.getElementById("mostrar");
+//registrar para o botao "mostrar" um ouvinte para o evento click,
+//que ao ser clicado vai mostrar a funcao mostrarOla
+mostrar.addEventListener("click" , mostrarOla);
